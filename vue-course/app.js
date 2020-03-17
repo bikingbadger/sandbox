c9fffd5245b1a972bcec559c57575738f7f2d131
Hilton Meyer <development@hiltonmeyer.com>
2020-03-17 15:24:05 +0200

    Add dynamic css

--- vue-course/app.js
+++ vue-course/app.js
@@ -3,13 +3,20 @@ new Vue({
   data: {
     counter: 0,
     secondCounter: 0,
-    randomNumber: 0
+    randomNumber: 0,
+    redSelected: false,
   },
   computed: {
     output: function() {
       console.log('Computed Call');
       return this.counter > 5 ? 'Greater than 5' : 'Less than 5';
     },
+    divClasses: function() {
+      return {
+        red: this.redSelected,
+        blue: !this.redSelected
+      }
+    }
   },
   methods: {
     result: function() {
@@ -19,6 +26,6 @@ new Vue({
     anotherResult: function() {
       console.log('Another Method Call');
       return this.counter > 5 ? 'Greater than 5' : 'Less than 5';
-    }
+    },
   },
 });
