64f39f3a9a0c78c92996c85d450abeb16c460ae5
Hilton Meyer <development@hiltonmeyer.com>
2020-03-20 15:32:52 +0200

    Looping through a Object Array

--- vue-course/using-conditionals-and-rendering-lists/app.js
+++ vue-course/using-conditionals-and-rendering-lists/app.js
@@ -4,8 +4,8 @@ new Vue({
     show: true,
     ingredients: ['apple', 'pear', 'banana'],
     people: [
-      { name: 'Max', age: 45 },
-      { name: 'Bob', age: 34 },
+      { name: 'Max', age: 45 , color: 'red'},
+      { name: 'Bob', age: 34 , sport: 'cycling'},
     ],
   },
 });
