df624b845117b933a7af473011d097ad7f869c4f
Hilton Meyer <development@hiltonmeyer.com>
2020-03-20 15:20:13 +0200

    Show arrays in list using v-for

--- vue-course/using-conditionals-and-rendering-lists/app.js
+++ vue-course/using-conditionals-and-rendering-lists/app.js
@@ -2,5 +2,10 @@ new Vue({
   el: '#app',
   data: {
     show: true,
+    ingredients: ['apple', 'pear', 'banana'],
+    people: [
+      { name: 'Max', age: 45 },
+      { name: 'Bob', age: 34 },
+    ],
   },
 });
