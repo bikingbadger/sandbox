63fa631a5f558df1940443fce8ea7f0b35aac64b
Hilton Meyer <development@hiltonmeyer.com>
2020-04-02 22:04:01 +0300

    Create mock server layouts

---
+++ vue-course/vue-components/src/components/server/ServerDetails.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>Server Details are currently not updated</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ vue-course/vue-components/src/components/shared/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <footer>
+    <p>All servers managed centrally</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return { uptime: "23 days 13:22" };
+  }
+};
+</script>
+
+<style></style>
