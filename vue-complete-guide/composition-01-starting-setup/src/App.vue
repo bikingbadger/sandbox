0875ab2e5ab2ced71c769dd9c7eb9e21b84ff9e1
Hilton Meyer <development@hiltonmeyer.com>
2021-02-11 09:38:48 +0200

    feat(composition): Add computed and watch

--- vue-complete-guide/composition-01-starting-setup/src/App.vue
+++ vue-complete-guide/composition-01-starting-setup/src/App.vue
@@ -1,42 +1,74 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
+    <h2>{{ username }}</h2>
     <h3>{{ user.age }}</h3>
 
     <div>{{ task.description }}</div>
     <div>{{ task.complete }}</div>
+
+    <button @click="birthday">Birthday</button>
+    <div>
+      <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Surname" @input="setLastName" />
+    </div>
   </section>
 </template>
 
 <script>
-import { ref, reactive } from 'vue';
+import { ref, reactive, computed, watch } from 'vue';
 
 export default {
-  // data() {
-  //   return {
-  //     userName: 'Maximilian',
-  //   };
-  // },
   setup() {
     const user = ref({
       name: 'Hilton',
-      age: 39,
+      age: 39
     });
 
     const task = reactive({
       description: 'Learn Vue',
-      complete: false,
+      complete: false
+    });
+
+    const firstName = ref('');
+    const lastName = ref('');
+
+    watch(firstName, (newValue) => {
+      //console.log(newValue, oldValue);
+      user.value.name = newValue;
+    });
+
+    const username = computed(() => {
+      return (firstName.value || user.value.name) + ' ' + lastName.value;
     });
 
     setTimeout(() => {
       user.value.name = 'George';
       user.value.age = 21;
-
-      task.complete = true;
     }, 2000);
 
-    return { user, task };
-  },
+    function setNewAge() {
+      user.value.age++;
+      task.description = 'Celebrate Birthday';
+      task.complete = true;
+    }
+
+    function setFirstName(event) {
+      firstName.value = event.target.value;
+    }
+
+    function setLastName(event) {
+      lastName.value = event.target.value;
+    }
+
+    return {
+      user,
+      task,
+      birthday: setNewAge,
+      setFirstName,
+      setLastName,
+      username
+    };
+  }
 };
 </script>
 
